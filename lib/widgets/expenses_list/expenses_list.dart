f4fa39fbdc0933b70f03d228b0eb6ccaa5362cfd
amosteandai <amostendai6@gmail.com>
2025-02-16 07:05:06 +0200

    Using theme data in widgets

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -16,6 +17,12 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error,
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
